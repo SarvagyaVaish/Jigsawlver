3a3e69739bcdf93a0fc474245e0dedc1e74747fe
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-06-18 17:11:02 -0700

    adding square pieces and starting basic matlab project

---
+++ edge_color_comparison.m
@@ -0,0 +1,16 @@
+clc;clear;
+
+pieces_filenames = { ...
+    'puzzles/square_pieces/piece_1.jpg', ...
+    'puzzles/square_pieces/piece_2.jpg', ...
+    'puzzles/square_pieces/piece_3.jpg', ...
+    'puzzles/square_pieces/piece_4.jpg' ...
+    };
+
+% create a list of all pieces
+pieces = {};
+for piece_filename = pieces_filenames
+    new_piece = im2double(rgb2gray(imread(piece_filename{1})));
+    pieces{end+1} = puzzlePiece(new_piece);
+end
+
--- puzzles/symmetric/convert_piece_to_binary.m
+++
@@ -1,9 +0,0 @@
-thresh = 180;
-
-i = imread('piece_2.jpg');
-i(i >= thresh) = 255;
-i(i < thresh) = 0;
-figure
-imshow(i)
-
-imwrite(i, 'piece_2.jpg');
