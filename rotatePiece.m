77cf7d5dfe806b62140a0c7d0aa63016f380c557
Adam Campbell <akroyvenslaka@gmail.com>
2013-06-21 19:58:44 -0700

    fix stuff, yo

--- rotatePiece.m
+++ rotatePiece.m
@@ -2,7 +2,7 @@ function [rotated_piece] = rotatePiece(piece_to_rotate, angle)
 rotated_piece = puzzlePiece(imrotate(piece_to_rotate.Image, angle, 'nearest', 'crop'));
 
 for i = 1:4
-   rotated_piece.Corners(end+1) = rotatePoint(piece_to_rotate.Corners(i), size(piece_to_rotate.Image)/2, angle); 
+   rotated_piece.Corners(i, :) = rotatePoint(piece_to_rotate.Corners(i, :), size(piece_to_rotate.Image)/2, angle); 
 end
 
 end
